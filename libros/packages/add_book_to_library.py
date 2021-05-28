fe17c554cc5bd7383fd8e460de4ca05c2a1f510b
Alex Foxleigh <alex@alexfoxleigh.com>
2021-05-28 19:18:32 +0100

    add_book_to_library now updates database

--- libros/packages/add_book_to_library.py
+++ libros/packages/add_book_to_library.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python3
 
-
-import requests
 import pymongo
-import xml.etree.cElementTree as ET
-from pprint import pprint as pp
-
+from packages.get_dewey_category import get_category
+from packages.get_dewey_subcategory import get_subcategory
+from packages.get_book_from_oclc import get_book
 
 """ Adds a single book to the database
 
@@ -13,56 +11,53 @@ Usage:
 
     python3 add_book_to_library.py
 """
+def add_book_to_library():
+    client = pymongo.MongoClient('10.56.1.30', 27017)
+    libaro_db = client["libaro"]
+    books = libaro_db['books']
 
-oclc_endpoint = 'http://classify.oclc.org/classify2/Classify?summary=true'
+    barcode = input("Scan book barcode: ")
 
-client = pymongo.MongoClient('10.56.1.30', 27017)
-libaro = client["libaro"]
-books = libaro['books']
+    book_data = get_book(barcode)
 
-barcode = input("Scan barcode: ")
+    try:
+        # Get book
+        book = book_data[1].attrib
 
-complete_endpoint = f'{oclc_endpoint}&isbn={barcode}'
+        # Get dewey number
+        dewey = book_data[5][0][1].attrib['sfa']
+    except:
+        print('Unable to parse book data')
 
-prime_categories = {
-    ('000', 'Computer Science, Information, and General Works', 'blue'),
-    ('100', 'Philosophy and Psychology', 'red'),
-    ('200', 'Religion', 'black'),
-    ('300', 'Social Sciences', 'purple'),
-    ('400', 'Language', 'green'),
-    ('500', 'Science', 'orange'),
-    ('600', 'Technology', 'pink'),
-    ('700', 'Arts and Recreation', 'yellow'),
-    ('800', 'Literature', 'brown'),
-    ('900', 'History and Geography', 'white'),
-}
+    try:
+        _, cat_name, cat_colour = get_category(dewey)
+    except:
+        print('Unable to retrieve category')
+    try:
+        subcat_name = get_subcategory(dewey)
+    except:
+        print('Unable to retreive subcategory')
 
-# TODO: Regex barcode
-req = requests.get(complete_endpoint)
-root = ET.fromstring(req.content)
+    data = {
+        'isbn': barcode,
+        'title': book['title'],
+        'author': book['author'],
+        'ddc': dewey,
+        'category': cat_name,
+        'subcategory': subcat_name,
+        'colour': cat_colour,
+        'held_by': 'Library' # A book will be held by the library by when added
+    }
 
-# Get book data from API
-# TODO: add try/catch block
-book = root[1].attrib
+    try:
+        books.insert_one(data)
+        print(f'New book added to the library:\n {book["title"]}')
+        print(f'Please give it a {cat_colour} sticker and the number: "{dewey}"')
+        print(f'Once done, add the book to the "{cat_name}" section under "{subcat_name}"')
+        
 
-# Get dewey number
-dewey = root[5][0][1].attrib['sfa']
+    except:
+        print('Error posting to database')
 
-# TODO: Get dewey subcategory from db
-
-
-def get_category(dewey):
-    for pc in prime_categories:
-        if (pc[0][0] == dewey[0]):
-            return pc
-
-
-data = {
-    'title': book['title'],
-    'author': book['author'],
-    'dewey': dewey,
-    'category': get_category(dewey),
-}
-
-# TODO: Post into db
-print(pp(data))
+if __name__ == '__main__':
+    add_book_to_library()
