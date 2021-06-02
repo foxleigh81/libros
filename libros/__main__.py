d5f54f5f40462617a0fe45105975641aa36f55b1
Alex Foxleigh <alex@alexfoxleigh.com>
2021-06-02 12:57:03 +0100

    Barcode scanning is now part of the main package instead of repeated in every module and there is a now a line of text indicating which option was pressed

--- libros/__main__.py
+++ libros/__main__.py
@@ -4,11 +4,12 @@ import readchar
 
 from packages.print_welcome_message import welcome_message
 from packages.print_options_list import provide_options
-from packages.add_book_to_library import add_book_to_library
+from packages.add_book_to_library import add_book
 from packages.delete_book_from_library import delete_book
 from packages.find_a_book import find_book
 from packages.loan_book_from_library import loan_book
 from packages.return_book_to_library import return_book
+from packages.scan_book_barcode import scan_barcode
 from packages.clear_console import clear_console
 
 def main():
@@ -18,19 +19,24 @@ def main():
     option = readchar.readkey()
 
     if (option == '1'):
+        print('Find a book:\n')
         find_book()
         main()
     elif (option == '2'):
-        loan_book()
+        print('Loaning a book:\n')
+        loan_book(scan_barcode())
         main()
     elif (option == '3'):
-        return_book()
+        print('Returning a book:\n')
+        return_book(scan_barcode())
         main()
     elif (option == '4'):
-        add_book_to_library()
+        print('Adding a new book:\n')
+        add_book(scan_barcode())
         main()
     elif (option == '5'):
-        delete_book()
+        print('Deleting a book:\n')
+        delete_book(scan_barcode())
         main()
     elif (option == '@'):
         return
