4187879a95144e0890fa3a9b31d70d165c1a2ae6
Alex Foxleigh <alex@alexfoxleigh.com>
2021-06-03 13:13:53 +0100

    Didn't mean to commit unfinished test

--- libros/tests/test_print_options_list.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from packages.print_options_list import provide_options
-
-class TestProvideOptions(unittest.TestCase):
-    def test_provide_options(self):
-        result = provide_options()
-        self.assertEqual(result, '')
